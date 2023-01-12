df552a92309cfb642cfad13adb591ab5103249c2
Tom Twyman <ttwyman@vmware.com>
2023-01-12 16:30:03 -0500

    initial normalization of json data
    Signed-off-by: Tom Twyman <ttwyman@vmware.com>

--- parse_custom.py
+++ parse_custom.py
@@ -5,8 +5,46 @@ from pandas import json_normalize
 
 
 def workload_profiles(**kwargs):
-    json_data = kwargs['parsed_data'] 
+    parsed__data = kwargs['parsed_data'] 
     profile_config = kwargs['profile_config']
-    pd.json_normalize(json_data)
-    recommendation_df = pd.read_json(json_data)
+
+    json_data = json.loads(parsed__data)
+    # print(json.dumps(json_data, indent = 4))
+
+    # recommendation_df = pd.json_normalize(json_data['workloadProfiles'], record_path=['vmList'])
+    recommendation_df = pd.json_normalize(json_data['workloadProfiles'])
+    # recommendation_df = pd.read_json(json_data)
     print(recommendation_df)
+    for col in recommendation_df.columns:
+        print(col)
+
+
+# vmId
+# vmGroupId
+# vmName
+# vmState
+# workloadProfileIndex
+# importedFileName
+# vmComputeInfo.vCpu
+# vmComputeInfo.vCpuOverall
+# vmComputeInfo.vCpuPerCore
+# vmComputeInfo.utilizationRatio
+# vmMemoryInfo.vRam
+# vmMemoryInfo.vRamConsumed
+# vmMemoryInfo.ramOverhead
+# vmMemoryInfo.utilizationRatio
+# vmStorageInfo.vmdkTotal
+# vmStorageInfo.vmdkUsed
+# vmStorageInfo.readIOPS
+# vmStorageInfo.writeIOPS
+
+    # df = pd.json_normalize(
+    #     data, 
+    #     record_path =['students'], 
+    #     meta=[
+    #         'class',
+    #         ['info', 'president'], 
+    #         ['info', 'contacts', 'tel']
+    #     ]
+    # )
+
